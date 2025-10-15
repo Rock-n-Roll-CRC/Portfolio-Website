c448cc5f8e51eefa8fd469e1ba681606fb11586f
Danil Dikhtyar <danil.dikhtyar@gmail.com>
2025-10-15 22:53:49 +0700

    🎉 Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+// @ts-check
+
+import { FlatCompat } from "@eslint/eslintrc";
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = defineConfig([
+  eslint.configs.recommended,
+  ...compat.extends("next/typescript"),
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  ...compat.extends("next/core-web-vitals", "prettier"),
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+]);
+
+export default eslintConfig;
