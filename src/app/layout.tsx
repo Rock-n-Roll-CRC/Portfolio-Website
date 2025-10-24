dfac6186bcd043ce872815afe519d7d74d7290dc
Danil Dikhtyar <danil.dikhtyar@gmail.com>
2025-10-24 13:57:17 +0700

    🔍 Add base metadata (title, icons, etc)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,29 @@
+import type { Metadata, Viewport } from "next";
+
+import { Roboto } from "next/font/google";
+
+import "@/styles/main.scss";
+
+const roboto = Roboto({ subsets: ["latin", "cyrillic", "vietnamese"] });
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://dikhtyar.dev"),
+  title: {
+    default: "Danil Dikhtyar | Software Engineer",
+    template: "%s | Danil Dikhtyar",
+  },
+  applicationName: "Dikhtyar.dev",
+  authors: { name: "Danil Dikhtyar", url: "https://dikhtyar.dev" },
+};
+
+export const viewport: Viewport = {
+  colorScheme: "light",
+  themeColor: "#f8f9fa",
+};
+
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.className}>
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home | Danil Dikhtyar",
+  description:
+    "Danil Dikhtyar - Software Engineer in Da Nang, Vietnam specializing in React, Next.js and TypeScript. View my full-stack projects and contact me!",
+};
+
 export default function HomePage() {
   return null;
 }
